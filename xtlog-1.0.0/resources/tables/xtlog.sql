f7d1f6317691c054d8df8377c7583df2a74b3eb2
Perry Clark <xtpclark@users.noreply.github.com>
2018-05-03 22:04:18 -0400

    update log

--- xtlog-1.0.0/resources/tables/xtlog.sql
+++ xtlog-1.0.0/resources/tables/xtlog.sql
@@ -1,18 +1,21 @@
-	-- table definition
+SELECT xt.create_table('xtlog', 'xtlog');
 
-select xt.create_table('xtlog', 'xtlog');
-select xt.add_column('xtlog','xtlog_id', 			'serial', 	'not null', 'xtlog');
-select xt.add_column('xtlog','xtlog_database', 		'text', 	'default current_database()', 'xtlog');
-select xt.add_column('xtlog','xtlog_username', 		'text', 	'', 'xtlog');
-select xt.add_column('xtlog','xtlog_client_addr', 	'inet', 	'default inet_client_addr()', 'xtlog');
-select xt.add_column('xtlog','xtlog_server_addr', 	'inet', 	'default inet_server_addr()', 'xtlog');
-select xt.add_column('xtlog','xtlog_server_port', 	'integer', 	'default inet_server_port()', 'xtlog');
-select xt.add_column('xtlog','xtlog_date', 			'timestamp with time zone', 'default now()', 'xtlog');
-select xt.add_column('xtlog','xtlog_status', 		'text', 	'', 			'xtlog');
-select xt.add_column('xtlog','xtlog_application', 	'text', 	'', 			'xtlog');
-select xt.add_column('xtlog','xtlog_backend_pid',   'integer', 'default pg_backend_pid()',	'xtlog');
-select xt.add_column('xtlog','xtlog_backend_start', 'timestamp with time zone','',			'xtlog');
-select xt.add_column('xtlog','xtlog_backend_stop',  'timestamp with time zone','',			'xtlog');
+ALTER TABLE xtlog.xtlog DISABLE TRIGGER ALL;
+
+SELECT
+ xt.add_column('xtlog','xtlog_id', 			'serial', 	'not null', 'xtlog'),
+ xt.add_column('xtlog','xtlog_database', 		'text', 	'default current_database()', 'xtlog'),
+ xt.add_column('xtlog','xtlog_username', 		'text', 	'', 'xtlog'),
+ xt.add_column('xtlog','xtlog_client_addr', 	'inet', 	'default inet_client_addr()', 'xtlog'),
+ xt.add_column('xtlog','xtlog_server_addr', 	'inet', 	'default inet_server_addr()', 'xtlog'),
+ xt.add_column('xtlog','xtlog_server_port', 	'integer', 	'default inet_server_port()', 'xtlog'),
+ xt.add_column('xtlog','xtlog_date', 			'timestamp with time zone', 'default now()', 'xtlog'),
+ xt.add_column('xtlog','xtlog_status', 		'text', 	'', 			'xtlog'),
+ xt.add_column('xtlog','xtlog_application', 	'text', 	'', 			'xtlog'),
+ xt.add_column('xtlog','xtlog_backend_pid',   'integer', 'default pg_backend_pid()',	'xtlog'),
+ xt.add_column('xtlog','xtlog_backend_start', 'timestamp with time zone','',			'xtlog'),
+ xt.add_column('xtlog','xtlog_backend_stop',  'timestamp with time zone','',			'xtlog'),
+ (TRUE);
 
 comment on table xtlog.xtlog is 'Client/Application Audit Logging';
 grant all on table xtlog.xtlog to admin;
@@ -22,3 +25,4 @@ grant all on xtlog.xtlog_xtlog_id_seq to admin;
 
 alter table xtlog.xtlog owner to admin;
 alter table xtlog.xtlog_xtlog_id_seq owner to admin;
+ALTER TABLE xtlog.xtlog ENABLE TRIGGER ALL;
